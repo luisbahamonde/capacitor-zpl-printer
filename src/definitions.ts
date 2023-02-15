0a955ecd04c4fd2c3de4262b9bc5c37a6711ed2e
Luis Bahamonde <luis@roocket.io>
2023-02-15 10:42:41 +0100

    - add print function

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,36 @@
 export interface CapacitorZplPrinterPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+    /**
+     *
+     * @param options
+     * @returns returns a promise
+     *
+     */
+    print(options: PrintOptions): Promise<any>
+
+    /**
+     *
+     * @param options
+     * @returns returns a promise
+     *
+     */
+    echo(options: { value: string }): Promise<{ value: string }>;
+}
+
+export interface PrintOptions {
+    /**
+     *
+     */
+    ip: string,
+    /**
+     *
+     */
+    port: number
+    /**
+     *
+     */
+    zpl: string
+    /**
+     *
+     */
+    prints?: number
 }
