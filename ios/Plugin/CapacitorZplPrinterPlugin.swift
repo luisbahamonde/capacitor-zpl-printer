6e4bbe4bbfa1a045e0aa96eaa05a6577a5e31f9a
Luis Bahamonde <luis@roocket.io>
2023-02-15 12:44:15 +0100

    - improve on ios code

--- ios/Plugin/CapacitorZplPrinterPlugin.swift
+++ ios/Plugin/CapacitorZplPrinterPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import SwiftSocket;
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -17,11 +18,26 @@ public class CapacitorZplPrinterPlugin: CAPPlugin {
         }
 
     @objc func print(_ call: CAPPluginCall) {
-        let ip = call.getString("ip")
-        let port = call.getNumber("port")
-        let zpl = call.getString("zpl")
-        let prints = call.getNumber("prints") ?? 1
-
+        let ip = call.getString("ip") ?? ""
+        let port = Int32(call.getInt("port", 9100))
+        let zpl = call.getString("zpl") ?? ""
+        let prints = call.getInt("prints") ?? 1
+        
+        let client = TCPClient(address: ip, port: port)
+        
+        let resul = client.connect(timeout: 10)
+        let print = client.send(string: zpl)
+        
+        //client.close();
+        
+        if(print.isSuccess){
+            call.resolve()
+        }
+        
+        if(print.isFailure){
+            call.reject("error en la impresion")
+        }
+        
         call.resolve()
     }
 }
